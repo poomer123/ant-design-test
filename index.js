064d5d7129ad8096cfcb625ba9ecdbb3a97cd7c5
ApiratChiwakanit <apirat.c@roundtwosolutions.com>
2018-02-15 15:26:58 +0700

    create basic table and table with filter

--- index.js
+++ index.js
@@ -2,15 +2,146 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { DatePicker } from 'antd';
+import { Table, Input, Button, Icon } from 'antd';
 
-function App() {
-  return (
-    <div style={{ margin: 100 }}>
-      <h1>AntDesign Demo</h1>
-      <hr /><br />
-      <DatePicker />
-    </div>
-  );
+const data = [{
+  key: '1',
+  name: 'John Brown',
+  age: 32,
+  address: 'New York No. 1 Lake Park',
+}, {
+  key: '2',
+  name: 'Joe Black',
+  age: 42,
+  address: 'London No. 1 Lake Park',
+}, {
+  key: '3',
+  name: 'Jim Green',
+  age: 32,
+  address: 'Sidney No. 1 Lake Park',
+}, {
+  key: '4',
+  name: 'Jim Red',
+  age: 32,
+  address: 'London No. 2 Lake Park',
+}];
+
+
+class App extends React.Component {
+  state = {
+    filterDropdownVisible: false,
+    data,
+    searchText: '',
+    filtered: false,
+  };
+  onInputChange = (e) => {
+    this.setState({ searchText: e.target.value });
+  }
+  onSearch = () => {
+    const { searchText } = this.state;
+    const reg = new RegExp(searchText, 'gi');
+    this.setState({
+      filterDropdownVisible: false,
+      filtered: !!searchText,
+      data: data.map((record) => {
+        const match = record.name.match(reg);
+        if (!match) {
+          return null;
+        }
+        return {
+          ...record,
+          name: (
+            <span>
+              {record.name.split(reg).map((text, i) => (
+                i > 0 ? [<span className="highlight">{match[0]}</span>, text] : text
+              ))}
+            </span>
+          ),
+        };
+      }).filter(record => !!record),
+    });
+  }
+  render() {
+    const columns = [{
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      filterDropdown: (
+        <div className="custom-filter-dropdown">
+          <Input
+            ref={ele => this.searchInput = ele}
+            placeholder="Search name"
+            value={this.state.searchText}
+            onChange={this.onInputChange}
+            onPressEnter={this.onSearch}
+          />
+          <Button type="primary" onClick={this.onSearch}>Search</Button>
+        </div>
+      ),
+      filterIcon: <Icon type="smile-o" style={{ color: this.state.filtered ? '#108ee9' : '#aaa' }} />,
+      filterDropdownVisible: this.state.filterDropdownVisible,
+      onFilterDropdownVisibleChange: (visible) => {
+        this.setState({
+          filterDropdownVisible: visible,
+        }, () => this.searchInput && this.searchInput.focus());
+      },
+    }, {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age',
+    }, {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+      filters: [{
+        text: 'London',
+        value: 'London',
+      }, {
+        text: 'New York',
+        value: 'New York',
+      }],
+      onFilter: (value, record) => record.address.indexOf(value) === 0,
+    }];
+
+
+    const dataSource1 = [{
+      key: '1',
+      name: 'Mike',
+      age: 32,
+      address: '10 Downing Street'
+    }, {
+      key: '2',
+      name: 'John',
+      age: 42,
+      address: '10 Downing Street'
+    }];
+    
+    const columns1 = [{
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    }, {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age',
+    }, {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+    }];
+    
+
+    return (
+      <div style={{ margin: 100 }}>
+        <h1>AntDesign Test</h1>
+        <hr /><br />
+        <DatePicker />
+        <Table dataSource={dataSource1} columns={columns1} />
+        <Table columns={columns} dataSource={this.state.data} />
+      </div>
+
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
